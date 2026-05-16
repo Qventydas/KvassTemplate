19ebb2e1196c27f6e6e2a5a797f7d6acc4c400cd
Qventydas <sulginanatala168@gmail.com>
2026-05-16 15:32:48 +0300

    базовый двумерный уровень, работа с объектами

--- Scripts/Levels/level.gd
+++ Scripts/Levels/level.gd
@@ -1,6 +1,15 @@
 extends Node
 class_name Level
+@export var object_groups: Dictionary[String,Node]
 
-# Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
 	Global.current_level = self
+
+func create_object(spawn: Node, group: String = "Objects"):
+	if object_groups.has(group):
+		object_groups[group].add_child(spawn)
+	else:
+		print("No such group:" + group)
+
+func get_all_objects(key:String = "Objects") -> Array:
+	return object_groups[key].get_children()
