42bf83b23f82fbd2bcea13a3e865c00fcefd702f
Qventydas <sulginanatala168@gmail.com>
2026-05-16 15:53:24 +0300

    objects to entities

--- Scripts/Levels/level.gd
+++ Scripts/Levels/level.gd
@@ -1,15 +1,15 @@
 extends Node
 class_name Level
-@export var object_groups: Dictionary[String,Node]
+@export var entity_groups: Dictionary[String,Node]
 
 func _enter_tree() -> void:
 	Global.current_level = self
 
-func create_object(spawn: Node, group: String = "Objects"):
-	if object_groups.has(group):
-		object_groups[group].add_child(spawn)
+func create_entity(spawn: Node, group: String = "Things"):
+	if entity_groups.has(group):
+		entity_groups[group].add_child(spawn)
 	else:
 		print("No such group:" + group)
 
-func get_all_objects(key:String = "Objects") -> Array:
-	return object_groups[key].get_children()
+func get_all_entities(key:String = "Things") -> Array:
+	return entity_groups[key].get_children()
