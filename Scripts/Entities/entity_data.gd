6e9dfbfdd701ddff84b33a85b7d787d6dbebcc53
Qventydas <sulginanatala168@gmail.com>
2026-06-26 16:59:39 +0300

    entity improvement

--- Scripts/Entities/entity_data.gd
+++ Scripts/Entities/entity_data.gd
@@ -1,9 +1,13 @@
 extends Node
 class_name EntityData
-
+var cached_owner: Node
+#var data_owner = get_data_owner()
 # Called when the node enters the scene tree for the first time.
 func get_data_owner() -> Node:
 	return get_parent().get_parent()
 
 func get_container() -> Node:
 	return get_parent()
+
+func data_init():
+	cached_owner = get_data_owner()
--- Scripts/Entities/entity_data_container.gd
+++ Scripts/Entities/entity_data_container.gd
@@ -1,6 +1,7 @@
 extends Node
-
+class_name EntityDataContainer
 @export var EntityDatas: Dictionary[String,EntityData] = {}
+
 
 func get_data(key: String) -> EntityData:
 	if key in EntityDatas.keys():
@@ -11,3 +12,4 @@ func get_data(key: String) -> EntityData:
 func _ready() -> void:
 	for child in get_children():
 		EntityDatas[child.name] = child
+		child.data_init()
